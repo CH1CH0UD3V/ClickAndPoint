04e74fb1e3dd11937a00775c054f38e3ead6507a
Chichou <ch1ch0ud3v@gmail.com>
2023-05-28 04:15:03 +0200

    update
    Vision for detect PlayerTag

--- Assets/Vision.cs
+++ Assets/Vision.cs
@@ -1,23 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Vision : MonoBehaviour
 {
-    PlayerBehaviour _player;
+    [SerializeField] string _mapLayer;
+    PlayerTag _target;
 
+    public PlayerTag Target { get { return _target; } }
+
+   
     private void OnTriggerEnter (Collider other)
     {
-
+        EnemyMovement (other);
     }
+
+    private void OnTriggerStay (Collider other)
+    {
+        EnemyMovement(other);
+    }
+
 
     void EnemyMovement (Collider other)
     {
-        _player = other.GetComponentInParent<PlayerBehaviour> (out PlayerBehaviour Player);
-        if (_player)
+        if (other.TryGetComponent<PlayerTag> (out PlayerTag Target))
         {
             var origin = transform.position;
-            var direction = Player.transform.position;
+            var direction = Target.transform.position - transform.position;
+            if (Physics.Raycast(origin, direction, direction.magnitude, LayerMask.GetMask(_mapLayer)))
+            {
+                Debug.DrawLine (origin, origin + direction, Color.red, 1f);
+            }
+            else
+            {
+                Debug.DrawLine (origin, origin + direction, Color.green, 1f);
+                _target = Target;
+            }
         }
+    }
+    
+    
+    
+    
+    private void OnTriggerExit (Collider other)
+    {
+
     }
 }
