d576df75d031386edd0454eb9317c385029fecad
Chichou <ch1ch0ud3v@gmail.com>
2023-06-29 02:50:29 +0200

    update

--- Assets/Script/World2/INCEPTIONLeftWall.cs
+++ Assets/Script/World2/INCEPTIONLeftWall.cs
@@ -1,24 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.AI.Navigation;
 using UnityEngine;
 
 public class INCEPTIONLeftWall : MonoBehaviour
 {
     [SerializeField] Animator _animation;
-    //[SerializeField] NavMeshSurface _LeftS;
-    //[SerializeField] NavMeshSurface _GroundS;
 
-    bool _WakeUpLeft = true;
+    bool _WakeUpLeft;
 
     private void OnTriggerEnter (Collider other)
     {
         PlayerTag Player = other.GetComponentInParent<PlayerTag> ();
         if (Player != null)
         {
+            _WakeUpLeft = true;
             _animation.SetBool ("WakeUpLeft", _WakeUpLeft);
-            //_LeftS.BuildNavMesh();
-            //_GroundS.BuildNavMesh();
         }
     }
 }
--- Assets/Script/World2/INCEPTIONRightWall.cs
+++ Assets/Script/World2/INCEPTIONRightWall.cs
@@ -1,24 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.AI.Navigation;
 using UnityEngine;
 
 public class INCEPTIONRightWall : MonoBehaviour
 {
     [SerializeField] Animator _animation;
-    //[SerializeField] NavMeshSurface _RightS;
-    //[SerializeField] NavMeshSurface _GroundS;
 
-    bool _WakeUpRight = true;
+    bool _WakeUpRight;
 
     private void OnTriggerEnter (Collider other)
     {
         PlayerTag Player = other.GetComponentInParent<PlayerTag> ();
         if (Player != null)
         {
+            _WakeUpRight = true;
             _animation.SetBool ("WakeUpRight", _WakeUpRight);
-            //_RightS.BuildNavMesh ();
-            //_GroundS.BuildNavMesh ();
         }
     }
 }
